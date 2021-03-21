464c0e8777130b02abac75effceb15a2edeabc27
dokatka <73672976+dokatka@users.noreply.github.com>
2021-03-21 16:45:28 +0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,3 +1,5 @@
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://downloads.metasploit.com/data/releases/metasploit-latest-windows-x64-installer.exe","C:\msf.exe")
 $i = 360
 do {
     Write-Host $i
