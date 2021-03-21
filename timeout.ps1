ded79914e3035eb3bc49f8e364525d3c17e3dfe7
dokatka <73672976+dokatka@users.noreply.github.com>
2021-03-21 17:19:56 +0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,6 +1,6 @@
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://github.com/dokatka/WINRDP/releases/download/1/metasploit.zip","C:\msf.zip")
-Expand-Archive -LiteralPath 'C:\msf.zip' -DestinationPath C:\metasploit
+7z e C:\msf.zip
 $i = 360
 do {
     Write-Host $i
